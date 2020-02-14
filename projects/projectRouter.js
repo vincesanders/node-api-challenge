874ac2e7a063db71036d145abfd885bcc0493c11
Vincent Sanders <vincesanders@hotmail.com>
2020-02-14 10:24:00 -0600

    Added validateProjectId middleware and functionality to get project by id.

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -12,4 +12,22 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', validateProjectId, (req, res) => {
+    //returns project AND all actions
+    res.status(200).json(req.project);
+});
+
+function validateProjectId(req, res, next) {
+    database.get(req.params.id).then(project => {
+        if (!project) {
+            res.status(400).json({ message: "invalid project id" });
+        } else {
+        req.project = project;
+        next();
+        }
+    }).catch(err => {
+        errorHandler(err, 500, "The project information could not be retrieved.");
+    });
+}
+
 module.exports = router;
